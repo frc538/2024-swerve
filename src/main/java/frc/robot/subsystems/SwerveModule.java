589ea15b366f16e0f5b1e66a543f266b10ff7655
Merrick538 <158779114+Merrick538@users.noreply.github.com>
2024-02-03 16:54:36 -0600

    Identifying module properties.

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,7 +4,45 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
 /** Add your docs here. */
 public class SwerveModule {
 
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turnMotor;
+    private final RelativeEncoder driveEncoder;
+    private final AbsoluteEncoder turnEncoder;
+    private final SparkPIDController drivePID;
+    private final SparkPIDController turnPID;
+
+    private double angularOffset = 0;
+    private SwerveModuleState desiredState = new SwerveModuleState(0.0, new Rotation2d());
+
+    public SwerveModule(int driveId, int turnId, double offset){
+        driveMotor = new CANSparkMax(driveId, MotorType.kBrushless);
+        turnMotor = new CANSparkMax(turnId, MotorType.kBrushless);
+
+        driveMotor.restoreFactoryDefaults();
+        turnMotor.restoreFactoryDefaults();
+
+        driveEncoder = driveMotor.getEncoder();
+        turnEncoder = turnMotor.getAbsoluteEncoder(Type.kDutyCycle);
+        
+        drivePID = driveMotor.getPIDController();
+        turnPID = turnMotor.getPIDController();
+
+        drivePID.setFeedbackDevice(driveEncoder);
+        turnPID.setFeedbackDevice(turnEncoder);
+
+        
+    }
 }
